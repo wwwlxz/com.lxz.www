dc770defc735e7abfa35a0f077b24881938c1a01
wwwlxz <564147911@qq.com>
2015-07-29 11:47:41 +0800

    添加说明
    添加说明

---
+++ src/test/java/com/lxz/test/Test01.java
@@ -0,0 +1,51 @@
+package com.lxz.test;
+
+class Person{
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
+
+class Student extends Person{
+	private String school;
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String toString(){
+		return "hello";
+	}
+}
+
+public class Test01 {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException{
+		Person person = (Person) getImplClass().newInstance();
+		System.out.println(Person.class.isInterface());
+		System.out.println(person.toString());
+	}
+	
+	private static Class<?> getImplClass(){
+		try{
+			return Class.forName("com.lxz.test.Student");
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
